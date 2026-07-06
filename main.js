1e1eb2d41a07fa28ddcaef2e4e5f6bd55449e2ec
Damsolm <126827933+Damsolm@users.noreply.github.com>
2026-07-06 21:03:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -476,13 +476,13 @@ const customerReviews = [
     {
         name: "Feyi Oluwasegun",
         role: "Report Developer",
-        avatar: "./assets/placeholder img.jpg",
+        avatar: "./placeholder img.jpg",
         review: "This Power BI course has been incredibly useful and practical. I absolutely love it because it focuses on real-world applications and gets the job done. The lessons are clear, hands-on, and easy to follow, making it easy to apply what I learn immediately in my work. It has significantly improved my understanding of Power BI and helped me build reports and dashboards with confidence. I highly recommend this course to anyone looking to gain practical Power BI skills. It delivered exactly what it promised."
     },
     {
         name: "Samuel Oye",
         role: "Financial Analyts",
-        avatar: "./assets/placeholder img.jpg",
+        avatar: "./placeholder img.jpg",
         review: "Well explanatory and easy to understand"
     }
 ];
