636be2b8a7e6441d420f73be12758c355f7c313a
Damsolm <126827933+Damsolm@users.noreply.github.com>
2026-07-09 02:31:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -337,7 +337,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         <h5>${prod.title}</h5>
                         <small>Price: <span style="font-weight:bold; color:green;">${price.current}</span> <del style="color:red; margin-left:5px;">${price.old}</del></small>
                     </div>
-                    <a href="javascript:void(0)" class="btn btn-primary" onclick="openDirectSelar('${prod.selarSlug}')">
+                    <a href="javascript:void(0)" class="specialist__whatsapp" onclick="openDirectSelar('${prod.selarSlug}')">
     View
 </a>
                 </article>
